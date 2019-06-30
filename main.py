c5e5df8c589a1cb90282194a90351c478bc83b5c
othellomizzen <51785789+othellomizzen@users.noreply.github.com>
2019-06-30 16:31:46 +0200

    Added parity bit support
    runHammingGeneral() now has an added mode argument to support Parity bit mode. Same for flipRandomBits().

--- main.py
+++ main.py
@@ -8,39 +8,38 @@ from datetime import datetime
 random.seed(datetime.now())
 
 
-
-"""
-This program shall have two modes; the user inputs a string, and either:
+""" This program has three modes; the user inputs a string, and either:
     - flip a random amount of bits of the raw data and display the corrupted data back as
         a text string, followed by using Hamming(7,4) to correct the corrupted message, or;
     - use a general Hamming code and corrupt->correct without displaying the wrong data as text.
-The reason for this is that I cannot think of a meaningful way to convert corrupted data back
-to text when not storing it as nibbles and using Hamming(7,4)
+    - add a parity bit, flip a random number of bits and parity check for errors
+The reason for this is that I cannot think of a meaningful way to convert corrupted 
+data back to text when not storing it as nibbles and using Hamming(7,4)
 
-Simply for the purposes of having a general Hamming code to correct data, the first option
+For the purposes of having a general Hamming code to correct data, the first option
 is completely unnecessary. However, it is simply fun to see on the screen what the corrupted 
-data would translate back into as text.
-"""
+data would translate back into as text. 
+See the end of this file for the runtime loop. """
 
-
-
-
-def flipRandomBits(bitList,n):
-    #note: randint and sample generates numbers up to AND INCLUDING the upper bound
-    #bitList is a list containing lists with the grouped bits:    
+def flipRandomBits(bitList,n,mode=None):
+    # flip a single bits in a random amount of groups of a message
+    # note: randint and sample generates numbers up to AND INCLUDING the upper bound
+    # bitList is a list containing lists with the grouped bits:    
     howManyRandom = random.randint(1,len(bitList)-1)
     groupRandom = random.sample(range(0,len(bitList)),howManyRandom)
     bitRandom = []
     for i in range(0,howManyRandom):
         if n==4:
-            bitRandom.append(random.choice([2,4,5,6]))
+            if mode == "p":
+                bitRandom.append(random.choice([0,1,2,3]))
+            else:
+                bitRandom.append(random.choice([2,4,5,6]))
         else:
             bitRandom.append(random.randint(0,len(bitList[0])-1))   
     flippedList = copy.deepcopy(bitList)
     for i in range(0,howManyRandom):
         flippedList[groupRandom[i]][bitRandom[i]] = (flippedList[groupRandom[i]][bitRandom[i]]+1)%2
     return flippedList,howManyRandom
-
 
 def StringToBitList(message,n):
     # converts text message to a list of lists containing the binary conversion,
@@ -56,7 +55,6 @@ def StringToBitList(message,n):
     for k in range(0,len(wholeBinaryString)//n):
         bitList.append([int(i) for i in wholeBinaryString[amountExtrabits+ k*n:amountExtrabits+ (k+1)*n]])
     return bitList
-
 
 def EncodedBitListToString(bitList,n):
     #convert a bitlist back to string
@@ -77,15 +75,14 @@ def EncodedBitListToString(bitList,n):
             
     return ''.join(chr(int(i,2)) for i in byteList)
 
-
-
-
-def runHammingGeneral(message,n):
-    G,H = BitMatrix.Generator(n)
+def runHammingGeneral(message,n,mode=None):
+    if mode == 'p':
+        G,H = BitMatrix.ParityMatrices()
+    else:
+        G,H = BitMatrix.Generator(n)
     dataToEncode = StringToBitList(message,n) #-> convert each letter to a byte, groups bits by n
-            # uneven divisions like: n=5, '011 11010'
+            # for uneven divisions like: n=5, '011 11010'
             # add 0's AT THE FRONT: -> '00011 11010'
-        
     #encode:
     encodedList = []
     for bitgroup in dataToEncode:
@@ -94,66 +91,68 @@ def runHammingGeneral(message,n):
         product = G*bitgroupVector
         encodedList.append(product.entries)
     
-    wrongdata,howManyRandom = flipRandomBits(encodedList,n)
-    print(howManyRandom, "bits were corrupted.")
-    if n== 4:
+    wrongdata,howManyRandom = flipRandomBits(encodedList,n,mode)
+    print(howManyRandom, "bit(s) were corrupted.")
+    if n== 4 and mode != 'p':
         print("Message after being corrupted: ")
         print(EncodedBitListToString(wrongdata,n) ,'\n')
             
-    # go through all groups and correct the wrong ones:
-    correctedList = []
-    for group in wrongdata:
-        bitgroupVector = BitMatrix(group,1)
-        syndromeVector = H*bitgroupVector
-        correctedVector = BitMatrix.correct(bitgroupVector,syndromeVector)
-        correctedList.append(correctedVector.entries)
-        
-    output = EncodedBitListToString(correctedList,n)
-            # convert back to a string, also remove added 0s from StringToBitList
+    if mode == 'p':
+        # Check how many errors have occured with parity check
+        howManyWrong = 0
+        for group in wrongdata:
+            bitgroupVector = BitMatrix(group,1)
+            syndromeVector = H*bitgroupVector
+            if not all(bit == 0 for bit in syndromeVector.entries):
+                howManyWrong += 1
+        print(howManyWrong, 'bits were successfully detected as incorrect.')
+    else:
+        # go through all groups and correct the wrong ones:
+        correctedList = []
+        for group in wrongdata:
+            bitgroupVector = BitMatrix(group,1)
+            syndromeVector = H*bitgroupVector
+            correctedVector = BitMatrix.correct(bitgroupVector,syndromeVector)
+            correctedList.append(correctedVector.entries)
             
-    # calculate how much time this whole damn thing took - put "toc" here? Look it up        
+        # convert back to a string, also remove added 0s from StringToBitList
+        output = EncodedBitListToString(correctedList,n) 
+        print('Message after being corrupted and corrected: ')
+        print(output, '\n')
     
-    print('Message after being corrupted and corrected: ')
-    print(output, '\n')
     
-
-
-
 
 """ ---------- RUNTIME ---------- """
-
-
 
 print("This program takes a user-provided string, changes a random bit in the input, "
       "then corrects it using Hamming codes. \n")
 running = True
 while running:
-    #print("Type 1 to use the Hamming(7,4) code "
-    #      "or type 2 to use a Hamming code of specified length: ")
-    print("In groups of how many bits should the data be split "
-          "(ex. use 4 for Hamming(7,4))?: ")
-    mode = input()
-    try:
-        n = int(mode)
-    except ValueError:
-        print("You did not input an integer! Try again: ")
-    else:
-        print("Input your message: ")
-        message = input()
-        start = datetime.now()
-        runHammingGeneral(message,n)
-        timedelta = datetime.now() - start
-        print('Time elapsed:', timedelta.total_seconds()*1000,'milliseconds.')
-        print("Do you wish to try another string? Input Y to go again, " 
-              "input anything else to quit: ")
-        replay = input()
-        if replay not in ["Y","y"]:
-            running = False
-        
+    print("Do you wish to add a parity bit or use Hamming codes? Input P for "
+          "parity, input anything else for Hamming: ")
+    parityMode = input()
+    n = 4
+    if parityMode != 'P' and parityMode != 'p':
+        needInt = True
+        while needInt:
+            print("In groups of how many bits should the data be split "
+              "(ex. use 4 for Hamming(7,4))?: ")
+            hammingMode = input()
+            try:
+                n = int(hammingMode)
+            except ValueError:
+                print("You did not input an integer! Try again: ")
+            else:
+                needInt = False
     
-
-
-
-
-
-
+    print("Input your message: ")
+    message = input()
+    start = datetime.now()
+    runHammingGeneral(message,n,parityMode.lower())
+    timedelta = datetime.now() - start
+    print('Time elapsed:', timedelta.total_seconds()*1000,'milliseconds.')
+    print("Do you wish to try another string? Input Y to go again, " 
+          "input anything else to quit: ")
+    replay = input()
+    if replay not in ["Y","y"]:
+        running = False
